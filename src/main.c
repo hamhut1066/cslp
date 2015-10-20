f492ac8d56e8329863137e4554a5ed698a4dbbb3
Hamish Hutchings <hamhut1066@gmail.com>
2015-10-20 20:39:23 +0100

    move library includes to top

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "whitedb/whitedb.h"
 
 #include "read_file.h"
-#include "whitedb/whitedb.h"
 
 void usage() {
   printf("Usage: cslp [filename]\n");
