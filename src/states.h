a3656a8b3fe0c2533290b6d1643f325d27537ef8
Hamish Hutchings <hamhut1066@gmail.com>
2015-10-20 21:02:55 +0100

    Add State Struct and Substructs

--- src/states.h
+++ src/states.h
@@ -27,3 +27,25 @@ typedef struct {
   struct BusDepartureEvent e;
 } Event;
 
+struct Stops {}; // Stops may be able to be marked on the ServiceNetwork
+struct Users {}; // State will be had
+struct MiniBuses {}; // These have state, and will need to be stored
+struct ServiceNetwork {}; // This needs to be thought about...
+
+/*
+ * This Defines a state of the program.
+ * Note: The values are _essentially_ immutable, (althought technically not possible)
+ *
+ * IDEA: implement this as a singly linked-list? giving us the ability to walk through all the states?... (TODO)
+ * i.e. State *next; &| State *prev;
+ * This could be stored in whitedb, allowing for creating a StateReference, or something similar.
+ */
+typedef struct {
+  /* defines the state no. this gives us the ablity to have a strictly ordered number of states */
+  int state_no;
+  struct Users *users;
+  struct MiniBuses *minibuses;
+  struct ServiceNetwork network;
+} State;
+
+// TODO: implement state specific methods (i.e... print_state)
