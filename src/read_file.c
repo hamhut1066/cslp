7fcfac0ae4bbfcc60b8ba362880cb0baa5d84c3a
Hamish Hutchings <hamhut1066@gmail.com>
2015-10-21 13:49:45 +0100

    cleanup of read_file.*

--- src/read_file.c
+++ src/read_file.c
@@ -9,22 +9,13 @@
 
 #include "read_file.h"
 
-/*
- * Key Value Struct
- * TODO: (Maybe pull out into generic functionality) (or maybe move into an in-memory db).
- */
-struct Kv {
-  char *key;
-  char *value;
-};
-
-void parse_map(State *state, config_setting_t *map_list) {
+void parse_map(struct Config *state, config_setting_t *map_list) {
   int dim; /* This is working on the assumption of a square matrix */
   int i, j, array_elem;
   config_setting_t *array;
 
   dim = config_setting_length(map_list);
-  state->config->dimension = dim;
+  state->dimension = dim;
 
   int **map = malloc(dim * sizeof(int*));
   /* populate and create the map as a 2-dimensional array */
@@ -41,93 +32,56 @@ void parse_map(State *state, config_setting_t *map_list) {
     }
   }
 
-  state->config->map = map;
+  state->map = map;
 }
 
-void parse_config(State *state, config_t *config) {
+void parse_config(struct Config *state, config_t *config) {
   double double_cfg;
   int int_cfg;
 
   if(config_lookup_int(config, "busCapacity", &int_cfg)) {
-    state->config->bus_capacity = int_cfg;
+    state->bus_capacity = int_cfg;
   }
 
   if(config_lookup_int(config, "boardingTime", &int_cfg)) {
-    state->config->boarding_time = int_cfg;
+    state->boarding_time = int_cfg;
   }
 
   if(config_lookup_float(config, "requestRate", &double_cfg)) {
-    state->config->request_rate = double_cfg;
+    state->request_rate = double_cfg;
   }
 
   if(config_lookup_float(config, "pickupInterval", &double_cfg)) {
-    state->config->pickup_interval = double_cfg;
+    state->pickup_interval = double_cfg;
   }
 
   if(config_lookup_int(config, "maxDelay", &int_cfg)) {
-    state->config->max_delay = int_cfg;
+    state->max_delay = int_cfg;
   }
 
   if(config_lookup_int(config, "noBuses", &int_cfg)) {
-    state->config->no_buses = int_cfg;
+    state->no_buses = int_cfg;
   }
 
   if(config_lookup_int(config, "noStops", &int_cfg)) {
-    state->config->no_stops = int_cfg;
+    state->no_stops = int_cfg;
   }
 
   if(config_lookup_int(config, "stopTime", &int_cfg)) {
-    state->config->stop_time = int_cfg;
+    state->stop_time = int_cfg;
   }
 
   config_setting_t *map_list = config_lookup(config, "map");
   parse_map(state, map_list);
 }
 
-/* void **read_config_file(char *buffer[], char* filename) { */
+void read_config_file(struct Config *config, char* filename) {
+  config_t cfg;
+  config_init(&cfg);
+  config_set_options(&cfg, CONFIG_OPTION_COLON_ASSIGNMENT_FOR_GROUPS);
+  config_set_options(&cfg, CONFIG_OPTION_COLON_ASSIGNMENT_FOR_NON_GROUPS);
 
-/*   FILE *f; */
-/*   int char_code; */
-/*   int i = 0; */
-/*   int j = 0; */
-/*   char line[100]; */
-/*   f = fopen(filename, "r"); */
-
-/*   if (f == NULL) { */
-/*     err(1, "%s", filename); */
-/*   } */
-
-/*   while ((char_code = fgetc(f)) != EOF) { */
-/*     char c = (char) char_code; */
-/*     line[j] = c; */
-
-/*     if(c == '\n') { */
-/*       // clear newline; */
-/*       line[j] = '\0'; */
-
-/*       int len = strlen(line); */
-/*       buffer[i] = calloc((len + 1), sizeof(char)); */
-/*       buffer[i] = strcpy(buffer[i], line); */
-/*       line[0] = '\0'; */
-/*       j = 0; */
-/*       i++; */
-/*     } else { */
-/*       line[j] = c; */
-/*       j++; */
-/*     } */
-
-/*   } */
-/*   if (!feof(f)) { */
-/*     err(1, "fgetln: <%s>", filename); */
-/*   } */
-/*   fclose(f); */
-/*   return NULL; */
-/* } */
-
-int read_config_file(config_t *config, char* filename) {
-  config_init(config);
-  config_set_options(config, CONFIG_OPTION_COLON_ASSIGNMENT_FOR_GROUPS);
-  config_set_options(config, CONFIG_OPTION_COLON_ASSIGNMENT_FOR_NON_GROUPS);
-
-  return config_read_file(config, filename);
+  if(config_read_file(&cfg, filename)) {
+    parse_config(config, &cfg);
+  }
 }
--- src/read_file.h
+++ src/read_file.h
@@ -6,8 +6,8 @@
 #define READ_FILE_H_
 #define BUFF_LEN 50
 
-int read_config_file(config_t *config, char* filename);
-void parse_config(State *state, config_t *config);
+void read_config_file(struct Config *config, char* filename);
+void parse_config(struct Config *state, config_t *config);
 
 #endif
 /* READ_FILE_H_ */
