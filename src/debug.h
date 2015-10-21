1a99abe87de265fb463a7997ef9d0e6d390321bd
Hamish Hutchings <hamhut1066@gmail.com>
2015-10-21 13:49:19 +0100

    add helper functions for printing out structs

--- src/debug.h
+++ src/debug.h
@@ -1,5 +1,14 @@
 #ifdef DEBUG
-#define DEBUG_PRINT(x) printf(x)
+#define DEBUG_PRINT(x) printf(x); printf("\n")
+#define PRINT_OPAQUE_STRUCT(p)  print_mem((p), sizeof(*(p)))
 #else
 # define DEBUG_PRINT(x) do {} while (0)
 #endif
+
+void print_mem(void const *vp, size_t n)
+{
+  unsigned char const *p = vp;
+  for (size_t i=0; i<n; i++)
+    printf("%02x\n", p[i]);
+  putchar('\n');
+};
