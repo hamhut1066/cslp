882107619c8dc4c473e5523139d3a87d157cac91
Hamish Hutchings <hamhut1066@gmail.com>
2015-10-21 12:48:55 +0100

    cleanup main file.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "whitedb/whitedb.h"
 
+#include "debug.h"
+
 #include "read_file.h"
 
 void usage() {
@@ -9,20 +11,22 @@ void usage() {
 }
 
 int main(int argc, char *argv[]) {
+  config_t config;
+  void *db;
+  void *rec, *rec2;
+  wg_int enc, enc2;
+
   if (argc < 2) {
     usage();
     return 0;
   }
-  char **buffer = malloc(BUFF_LEN * sizeof(char*));
-  read_config_file(buffer, argv[1]);
-  for (int i = 0; i < BUFF_LEN; i++) {
-    /* printf("%s\n", buffer[i]); */
-  }
 
-  void *db, *rec, *rec2;
-  wg_int enc, enc2;
+  DEBUG_PRINT("Hello World\n");
 
+  /* Initialise and allocate base objects. */
+  read_config_file(&config, argv[1]);
   db = wg_attach_database("1000", 2000000);
+
   rec = wg_create_record(db, 10);
   rec2 = wg_create_record(db, 2);
 
@@ -31,5 +35,9 @@ int main(int argc, char *argv[]) {
 
   wg_set_field(db, rec, 7, enc);
   wg_set_field(db, rec2, 0, enc2);
+
+  /* Cleanup of assigned variables */
+  config_destroy(&config);
+  /* wg_delete_local_database(db); */
   return 0;
 }
