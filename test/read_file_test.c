16b74bcc6017e99b44c104d968fedb2492774837
Hamish Hutchings <hamhut1066@gmail.com>
2015-12-07 22:41:51 +0000

    add test for re-arranged input

--- test/read_file_test.c
+++ test/read_file_test.c
@@ -7,8 +7,8 @@
 
 #include "../src/read_file.h"
 
-#define TEST_FILE "./test/fs/file.in"
 #define SAMPLE_INPUT "./test/fs/input.in"
+#define BAD_INPUT  "./test/fs/bad_config_file.in"
 
 
 void parse_config_test(CuTest *tc) {
@@ -18,8 +18,6 @@ void parse_config_test(CuTest *tc) {
   read_config_file(config, SAMPLE_INPUT);
   set_initial_state(state, config);
 
-  print_config(state->config);
-
   CuAssertIntEquals(tc, config->bus_capacity, state->config->bus_capacity);
   CuAssertIntEquals(tc, config->bus_capacity, 12);
 
@@ -27,8 +25,24 @@ void parse_config_test(CuTest *tc) {
   CuAssertIntEquals(tc, config->map[1][2], 5);
   CuAssertIntEquals(tc, config->map[3][2], -1);
 
+  destroy_config(config);
   destroy_state(state);
 
+}
+
+void parse_bad_config_test(CuTest *tc) {
+  struct Config *config = get_initial_config();
+
+  read_config_file(config, BAD_INPUT);
+
+  CuAssertIntEquals(tc, config->bus_capacity, 12);
+
+  CuAssertIntEquals(tc, config->map[0][0], 0);
+  CuAssertIntEquals(tc, config->map[1][2], 5);
+  CuAssertIntEquals(tc, config->map[3][2], -1);
+
+
+  destroy_config(config);
 }
 
 
@@ -36,5 +50,6 @@ CuSuite* ReadFileGetSuite() {
   CuSuite* suite = CuSuiteNew();
   /* SUITE_ADD_TEST(suite, read_file_test); */
   SUITE_ADD_TEST(suite, parse_config_test);
+  SUITE_ADD_TEST(suite, parse_bad_config_test);
   return suite;
 }
