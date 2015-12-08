5e94a0d3ceec6a0eaeaaeca491f760912678aaa4
Hamish Hutchings <hamhut1066@gmail.com>
2015-12-08 21:15:17 +0000

    Initial implementation of the controller

---
+++ src/controller.c
@@ -0,0 +1,11 @@
+#include "controller.h"
+#include "states.h"
+
+/*
+ * This method sets up some basic things, then gets to work on the problem.
+ */
+void run_experiment(State *state) {
+  State *initial_state = state;
+
+  print_state(initial_state);
+}
---
+++ src/controller.h
@@ -0,0 +1,8 @@
+#ifndef _CONTROLLER_H_
+#define _CONTROLLER_H_
+
+#include "states.h"
+
+void run_experiment(State *state);
+
+#endif
