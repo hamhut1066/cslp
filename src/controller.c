d62b694cdbb6f777e81029bba369e27adcb419ac
Hamish Hutchings <hamhut1066@gmail.com>
2015-12-09 23:18:05 +0000

    add stochastic distribution of users arriving

--- src/controller.c
+++ src/controller.c
@@ -1,20 +1,34 @@
 #include "controller.h"
 #include "states.h"
 
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+double uniform_deviate (int seed) {
+  return seed * (1.0 / (RAND_MAX + 1.0));
+}
 
 void run_iteration(State *state) {
-  state->time = state->time + 10000;
+  double r;
+
+  do
+    r = uniform_deviate(rand());
+  while (r == 0.0);
+
+  int next_passenger = (int) (-log(r)* state->config->request_rate);
+  state->time = state->time + next_passenger;
 }
 
 /*
  * This method sets up some basic things, then gets to work on the problem.
  */
 void run_experiment(State *state) {
-  State *initial_state = state;
+  srand((int)time(NULL));
+  rand(); /* calling once because first value is always the same */
 
 
   while (state->time < state->config->stop_time) {
     run_iteration(state);
   }
-  print_state(initial_state);
 }
