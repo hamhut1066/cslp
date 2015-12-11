57be2256629ccadf2b0028022c3c0cff54b26b1c
Hamish Hutchings <hamhut1066@gmail.com>
2015-12-11 15:05:53 +0000

    add tests for the controller.
    These tests start actually testing more functionality in the event loop

--- test/controller_test.c
+++ test/controller_test.c
@@ -8,7 +8,7 @@
 #include "../src/read_file.h"
 
 /* generates a state with the default test file */
-State *test_state() {
+struct State *test_state() {
   struct Config config;
 
   read_config_file(&config, "./test/fs/input.in");
@@ -18,7 +18,7 @@ State *test_state() {
 
 
 void initial_iteration_test(CuTest *tc) {
-  State *state = test_state();
+  struct State *state = test_state();
 
   CuAssertIntEquals(tc, 0, state->no);
   CuAssertIntEquals(tc, 0, state->time);
@@ -27,16 +27,33 @@ void initial_iteration_test(CuTest *tc) {
 
 
 void end_of_experiment_test(CuTest *tc) {
-  State *state = test_state();
+  struct State *state = test_state();
+  struct Stats *stats;
 
-  run_experiment(state);
+  stats = run_experiment(state);
 
-  CuAssertIntEquals(tc, 1, state->time >= state->config->stop_time);
+  CuAssertIntEquals(tc, 1, stats->state->time >= stats->state->config->stop_time);
+  CuAssertIntEquals(tc, 1, stats->state->no >= 1);
+}
+
+void tick_increments_state(CuTest *tc) {
+  struct State *state = test_state();
+  struct State *original_state = state;
+
+  state = tick(state);
+
+  CuAssertPtrEquals(tc, original_state, state->last);
+  CuAssertIntEquals(tc, 1, state->no);
+  CuAssertIntEquals(tc, 0, original_state->no);
+
+  destroy_state(state);
 }
 
 CuSuite* ControllerGetSuite(void) {
   CuSuite* suite = CuSuiteNew();
   SUITE_ADD_TEST(suite, initial_iteration_test);
   SUITE_ADD_TEST(suite, end_of_experiment_test);
+  SUITE_ADD_TEST(suite, initial_iteration_test);
+  SUITE_ADD_TEST(suite, tick_increments_state);
   return suite;
 }
