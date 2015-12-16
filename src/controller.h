7b3b683bc9bc061e594dea6670fa9d2118e1ca27
Hamish Hutchings <hamhut1066@gmail.com>
2015-12-16 20:58:05 +0000

    add export to method for testing

--- src/controller.h
+++ src/controller.h
@@ -10,4 +10,5 @@ struct State *tick(struct State *state);
 char *format_time(int time);
 struct Route *shortest_route(struct State *state, int source, int destination);
 int min_vertex(int *vertex, int *dist, int length);
+int stop_adjacent(struct State *state, int source, int dest);
 #endif
